48a23483b2f03d7db02e9c3536ce4befb8640848
Dmitriy <superdima05@gmail.com>
2019-12-27 01:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import deezer
 from transliterate import translit
 import os
 from mutagen.flac import FLAC
+import appex
 
 
 #SOON
@@ -14,6 +15,8 @@ auth_password = 'DEEZER PASSWORD'
 client = deezer.Client()
 
 m = 0
+id = 0
+
 def getmode():
     global m
     print("Select mode:")
@@ -50,54 +53,86 @@ def download(trlist):
     print("Done")
 
 def mode():
-    global m
-    tracks = []
-    if(m == 1):
-        try:
-            trid = int(input("Enter ID: "))
-            url = client.get_track(trid)
-            temp = []
-            temp.append(trid)
-            temp.append(url)
-            tracks.append(temp)
-        except Exception:
-            mode()
-    if(m == 2):
-        try:
-            plid = int(input("Enter ID: "))
-            url = client.get_playlist(plid)
-            for i in url.tracks:
-                temp = []
-                temp.append(i.id)
-                temp.append(i)
-                tracks.append(temp)
-        except Exception:
-            mode()
-    if(m == 3):
-        try:
-            alid = int(input("Enter ID: "))
-            url = client.get_album(alid)
-            for i in url.tracks:
-                temp = []
-                temp.append(i.id)
-                temp.append(i)
-                tracks.append(temp)
-        except Exception:
-            mode()
-    if(m == 4):
-        query = input("Search: ")
-        result = client.search(query)
-        for i in result:
-            print(i.artist.name+" - "+i.title+" | Album name: "+i.album.title)
-    if(m != 4):
-        return tracks
+	global m, id
+	tracks = []
+	if(m == 1):
+		try:
+			if(id == 0):
+				trid = int(input("Enter ID: "))
+			else:
+				trid = id
+			url = client.get_track(trid)
+			temp = []
+			temp.append(trid)
+			temp.append(url)
+			tracks.append(temp)
+		except Exception:
+			mode()
+	if(m == 2):
+		try:
+			if(id == 0):
+				plid = int(input("Enter ID: "))
+			else:
+				plid = id
+			url = client.get_playlist(plid)
+			for i in url.tracks:
+				temp = []
+				temp.append(i.id)
+				temp.append(i)
+				tracks.append(temp)
+		except Exception:
+			mode()
+	if(m == 3):
+		try:
+			if(id == 0):
+				alid = int(input("Enter ID: "))
+			else:
+				alid = id
+			url = client.get_album(alid)
+			for i in url.tracks:
+				temp = []
+				temp.append(i.id)
+				temp.append(i)
+				tracks.append(temp)
+		except Exception:
+			mode()
+	if(m == 4):
+		query = input("Search: ")
+		result = client.search(query)
+		for i in result:
+			print(i.artist.name+" - "+i.title+" | Album name: "+i.album.title)
+	if(m != 4):
+		return tracks
 
 def start():
-    getmode()
-    trlist = mode()
-    if(m != 4):
-        download(trlist)
-    again()
+	global m, id
+	if appex.is_running_extension():
+		url = appex.get_url()
+		if not url:
+			print('No input URL found.')
+			exit()
+		else:
+			if(url.replace("track", "") != url):
+				m = 1
+				id = url.split("/")
+				id = id[len(id)-1]
+			elif(url.replace("playlist", "") != url):
+				m = 2
+				id = url.split("/")
+				id = id[len(id)-1]
+			elif(url.replace("album", "") != url):
+				m = 3
+				id = url.split("/")
+				id = id[len(id)-1]
+			else:
+				print("No method, use search")
+				m = 4
+	else:
+		getmode()
+	trlist = mode()
+	if(m != 4):
+		download(trlist)
+	again()
 
 def again():
     want = input("Do you want to continue [0/1]: ")
